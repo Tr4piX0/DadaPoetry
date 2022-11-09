92aa12f84db51c7e8764cd995eef8c9d419270cb
arnaldorusso <arnaldo.russo@giscaruso.com.br>
2022-11-09 15:38:37 -0300

    server including save in database

--- dadapoetry/backend/server.py
+++ dadapoetry/backend/server.py
@@ -1,34 +1,48 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+import sqlite3
+from flask import (Flask, flash, request, redirect, url_for,
+                   send_from_directory, render_template)
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/home/arnaldo/Imagens'
+UPLOAD_FOLDER = '/home/arnaldo/Imagens/imgs/'
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def getDbConnection():
+    conn = sqlite3.connect('dada.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def uploadFile():
     if request.method == 'POST':
+        file = request.files['file']
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            conn = getDbConnection()
+            conn.execute("INSERT INTO dada (filename, content) VALUES (?, ?)",
+                (filename, path)
+            )
+            conn.close()
+            # return redirect(url_for('downloadFile', name=filename))
+            return redirect(url_for('downloadFile', name=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -39,9 +53,9 @@ def upload_file():
     </form>
     '''
 
-
 @app.route('/uploads/<name>')
-def download_file(name):
+def downloadFile(name):
+
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 if __name__ == '__main__':
